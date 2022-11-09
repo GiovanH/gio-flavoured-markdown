723d5a614dfbf248d144f16cbde7cefdc1323f60
Glitch (glitch-blank-website) <none>
2022-11-09 04:04:43 +0000

    🍫🍭 Checkpoint ./start.sh:50827847/125 ./requirements.txt:50827847/59 ./requirements3.txt:50827847/51 ./server.py:50827847/34 ./templates/index.html:50827847/665 ./glitch.json:50827847/2

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from flask_mde import Mde
+# from flask_mde import Mde
 
 app = Flask(__name__)
-mde = Mde(app)
+# mde = Mde(app)
 
 @app.route("/")
 def home():
+  rendered = "Hello!"
   return render_template("index.html")
 
 if __name__ == "__main__":
--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements3.txt \
+ & git clone https://github.com/GiovanH/mdexts.git \
+ & git clone https://github.com/GiovanH/peliplugins.git
+ 
+wait
+
 python3 server.py
