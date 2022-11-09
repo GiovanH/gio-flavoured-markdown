cc869fa84134737c8266883c3be5bca3e37b6873
Glitch (glitch-blank-website) <none>
2022-11-09 03:52:53 +0000

    💄📖 Checkpoint ./start.sh:50827847/73 ./server.py:50827847/1126 ./templates/index.html:50827847/1863 ./Requirements.txt:50827847/5 ./requirements.txt:50827847/10

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask_mde import Mde
+
+app = Flask(__name__)
+mde = Mde(app)
+
+@app.route("/")
+def home():
+  return render_template("index.html")
+
+if __name__ == "__main__":
+  app.run()
