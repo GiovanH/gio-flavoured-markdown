53913da9140ae0b3424dde7e9d0104501372ce79
Glitch (glitch-blank-website) <none>
2022-11-10 05:23:15 +0000

    🌮🛀 Checkpoint ./templates/index.html:50827847/1793 ./static/preview.js:50827847/232 ./static/style.css:50827847/128

--- static/preview.js
+++ static/preview.js
@@ -1,5 +1,16 @@
-function updateiframe(mdArea, iframe_id) {
+
+function debounce(func, timeout = 300){
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  };
+}
+
+function _updateiframe(mdArea, iframe_id) {
   try {
     document.getElementById(iframe_id).setAttribute('src', "/render?q=" + encodeURIComponent(mdArea.value))
   } catch {}
 }
+
+const updateiframe = debounce(_updateiframe, 500)
