5388b0d3b8c4e3a4388046a92b04c04941f91b56
Glitch (glitch-blank-website) <none>
2022-11-10 05:11:18 +0000

    🐿🍈 Checkpoint ./templates/markdown.html:50827847/282 ./static/preview.js:50827847/2 ./templates/index.html:50827847/3929

--- static/preview.js
+++ static/preview.js
@@ -1,5 +1,5 @@
 function updateiframe(mdArea, iframe_id) {
   try {
-  document.getElementById(iframe_id).setAttribute('src', "/render?q=" + encodeURIComponent(mdArea.value))
+    document.getElementById(iframe_id).setAttribute('src', "/render?q=" + encodeURIComponent(mdArea.value))
   } catch {}
 }
