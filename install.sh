3123d65403b6e13091a5f0d7dd135238fca9d7d2
Glitch (glitch-blank-website) <none>
2022-11-11 06:45:39 +0000

    👣🔨 Checkpoint ./static/default_input.md:50827847/583 ./install.sh:50827847/296

--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-git_clone_or_update() {
-  eg Clone a repo, or pull-update it if it already exists.
+git config --global user.name "Glitch Client"
+git config --global user.email gio-flavoured-markdown@glitch.me
+
+git_clone_or_update_force() {
+  # Clone a repo, or pull-update it if it already exists.
   repo=$1
   autodest=${repo##*/} # ## delete longest match of pattern from start
   autodest=${autodest%.git} # %  delete shortest match of pattern from end
   gitdest=${2:-$autodest}
   git clone $repo $gitdest \
-    || (pushd $gitdest && git pull && popd)
+    || (pushd $gitdest && git fetch; git reset --hard origin; git pull && popd)
   return 0
 }
 
 python3 -m pip install -r requirements3.txt \
  & wget -nc 'https://raw.githubusercontent.com/marcel-dancak/lz-string-python/master/lzstring.py' \
- & git_clone_or_update https://github.com/GiovanH/mdexts.git \
- & git_clone_or_update https://github.com/GiovanH/peliplugins.git
+ & git_clone_or_update_force https://github.com/GiovanH/mdexts.git \
+ & git_clone_or_update_force https://github.com/GiovanH/peliplugins.git
  
 wait
